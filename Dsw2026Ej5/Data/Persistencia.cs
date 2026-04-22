68e030e1abd076cd458adb935866747f5d1b711b
lopezsardip <lopezsardip@gmail.com>
2026-04-21 23:30:36 -0400

    solucionar agregar vehiculos, ya funcionando

--- Dsw2026Ej5/Data/Persistencia.cs
+++ Dsw2026Ej5/Data/Persistencia.cs
@@ -48,10 +48,24 @@ public class Persistencia
     {
         return Vehiculos;
     }
+    public static List<Sucursal> GetSucursales()
+    {
+        return Sucursales;
+    }
+
+    public static Sucursal? GetSucursalPorCodigo(string codigo)
+    {
+        return Sucursales.Find(s => s.GetCodigo() == codigo);
+    }
 
     public static Vehiculo? GetVehiculo(string patente)
     {
         return Vehiculos.Find(v => v.GetPatente() == patente);
+    }
+
+    public static void AgregarVehiculo(Vehiculo vehiculo)
+    {
+        Vehiculos.Add(vehiculo);
     }
 
     public static void InicializarDatos()
